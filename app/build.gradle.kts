c2f4c1a32620e7ae8b13e5e959a0c08c88a6d37b
ferdianrra <ferdianrra@student.gunadarma.ac.id>
2024-10-26 01:33:32 +0700

    change min sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dicoding.newsapp"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
